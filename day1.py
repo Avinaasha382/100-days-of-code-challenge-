130010f7a337d1b2419a3af347f1d9b78ef16245
Avinaasha382 <avinaasha382@gmail.com>
2023-12-27 11:27:16 +0530

    Day-1 of 100 days of code

--- day1.py
+++ day1.py
@@ -1,1 +1,9 @@
-print("Hi")
+# generate a band name:
+
+# On day-1 I learnt what are the basic data types in Python, using print statements,input statements and string concatenation
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
+

3eb92f8ce0733c9893223ebcd46fa445cbde5f88
Avinaasha382 <avinaasha382@gmail.com>
2023-12-27 11:59:40 +0530

    Day-4 of 100 days of coding challenge

---
+++ day4.py
@@ -0,0 +1,62 @@
+# day 4:
+           # Randomisation and the random module in Python
+           #   Concept of mutability and immutability
+           # Lists in Python
+
+# Todays Project: Rock Paper Scissors game with the computer
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if(user_choice>2):
+  print("Invalid selection")
+else:
+  print(game_images[user_choice])
+
+  computer_choice = random.randint(0, 2)
+  print("Computer chose:")
+  print(game_images[computer_choice])
+
+  if user_choice >= 3 or user_choice < 0: 
+    print("You typed an invalid number, you lose!") 
+  elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+  elif computer_choice == 0 and user_choice == 2:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win!")
+  elif computer_choice == user_choice:
+    print("It's a draw")
+
+

d8e095be746a8af3d9d920daf4ec405df8b61ffa
Avinaasha382 <avinaasha382@gmail.com>
2024-02-05 15:13:57 +0530

    GUI MILE TO KM CONVERTOR USING TKINTER

--- Day-27/main.py
+++ Day-27/main.py
@@ -3,27 +3,28 @@ from tkinter import *
 window = Tk()
 window.minsize(width = 300 , height = 100)
 window.title("Miles to km convertor")
-
+window.config(padx=20,pady=20)
 def convert():
-    val = (float)(b1.get())
+    val = (float)(mileVal.get())
     newVal = val*1.60934
-    l3.config(text=str(newVal)+" km")
+    outPut.config(text=str(newVal)+" km")
 #logic:
 
-l1 = Label(text="is equal to")
-l1.place(x=30,y=50)
-l1.config(padx=20,pady=20)
-b1 = Entry()
-b1.place(x=50,y=30)
+mileVal = Entry()
+mileVal.grid(column=1,row=0)
 
-l2 = Label(text="Miles")
-l2.place(x=150,y=15)
-l2.config(padx=20,pady=20)
+mileLabel = Label(text="Miles")
+mileLabel.grid(column=2,row=0)
 
-l3 = Label(text="0")
-l3.place(x=120,y=70)
+isEqlLabel = Label(text="is equal to")
+isEqlLabel.grid(column=0,row=1)
 
-b = Button(text="Convert",command=convert)
-b.place(y=90,x=130)
-#running the window:
+outPut = Label(text="0")
+outPut.grid(column=1,row=1)
+
+kmLabel = Label(text="Km")
+kmLabel.grid(column=2,row=1)
+
+button = Button(text="Calculate",command=convert)
+button.grid(column=1,row=2)
 window.mainloop()
